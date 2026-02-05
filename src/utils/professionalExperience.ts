aec20eb8c5bad5c9c99922ae993b175645bc5c94
Adrian Rangel <adrianrangelargu@gmail.com>
2026-02-04 21:09:23 -0400

    feat: added About page with professional experience, skills, and CTA sections

---
+++ src/utils/professionalExperience.ts
@@ -0,0 +1,32 @@
+export const professionalExperience = [
+  {
+    title: "Frontend Web Developer",
+    company: "Sempiterno Group",
+    location: "Venezuela · Remote",
+    description: "My role at Sempiterno Group as a Frontend Web Developer focuses on significantly contributing to the scalability and robustness of web solutions. One of my key responsibilities is Full-Stack integration, where I developed a seamless consumption of multiple backend services (REST), ensuring a smooth data flow for the end user. Additionally, I performed strategic refactoring using design patterns that not only reduced the project's technical debt but also improved development speed by 15% for new features. My work is always focused on usability, applying UX/UI knowledge to enhance user comfort and experience.",
+    techs: ["React", "NextJS", "TypeScript", "TailwindCSS", "REST", "UX/UI", "Design Patterns"],
+    startDate: "Apr 2025",
+    endDate: "Jan 2026",
+    duration: "10 months"
+  },
+  {
+    title: "Junior Frontend Developer",
+    company: "Municipality of Miranda",
+    location: "Trujillo State, Venezuela",
+    description: "At the Municipality of Miranda, I collaborated on developing a key application for employee management. My work focused on improving the operational efficiency of the area, successfully reducing data processing and validation time by at least 10% through workflow optimization. From a technical standpoint, I was responsible for adjusting and redesigning interfaces, implementing robust CSS architectures like BEM and using Styled Components. This standardization not only modernized the design but also increased CSS code maintainability and facilitated the incorporation of future changes.",
+    techs: ["Frontend", "Astro", "CSS", "BEM", "Styled Components"],
+    startDate: "Jan 2024",
+    endDate: "Feb 2025",
+    duration: "1 year 1 month"
+  },
+  {
+    title: "Junior Frontend Developer",
+    company: "Quasar",
+    location: "Trujillo State, Venezuela · On-site",
+    description: "At Quasar, my role was fundamental in developing an essential interface and web application for managing a small business. My work had a direct impact on operational efficiency, allowing the company to manage inventory and facilitate the importation of data from the new digital platform. In the technological field, I played a crucial role in implementing new technologies like Astro. Thanks to this strategic migration, we achieved a documented reduction in application load time (LCP) of up to 30%, drastically improving performance and user experience, a key factor for business success.",
+    techs: ["TailwindCSS", "Astro", "Performance Optimization", "Axios", "Svelte"],
+    startDate: "Apr 2023",
+    endDate: "Aug 2023",
+    duration: "5 months"
+  }
+]
